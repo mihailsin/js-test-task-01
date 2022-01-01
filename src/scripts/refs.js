17762b8a68f32151fe31173c720516136001b129
Mihail Sinkov <mihailsin88@gmail.com>
2022-01-01 14:29:31 +0200

    adds styles

--- src/scripts/refs.js
+++ src/scripts/refs.js
@@ -2,4 +2,6 @@ export const refs = {
   currencyForm: document.querySelector('.latest-rates'),
   selectField: document.querySelector('#base-rate'),
   currenciesList: document.querySelector('.currency__list'),
+  ratesRef: document.querySelector('#rates-trigger'),
+  calculatorRef: document.querySelector('#calculator-trigger'),
 };
