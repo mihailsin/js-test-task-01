43cda57f2dc4539f83c92015627a6a32c6f8d997
Mihail Sinkov <mihailsin88@gmail.com>
2022-01-02 18:01:41 +0200

    adds positioning for calculatorForm elements

--- src/scripts/triggers.js
+++ src/scripts/triggers.js
@@ -4,10 +4,14 @@ refs.ratesRef.addEventListener('click', e => {
   e.preventDefault();
   refs.currenciesRates.style.display = 'block';
   refs.currenciesCalculator.style.display = 'none';
+  refs.ratesRef.classList.add('is-active');
+  refs.calculatorRef.classList.remove('is-active');
 });
 
 refs.calculatorRef.addEventListener('click', e => {
   e.preventDefault();
   refs.currenciesCalculator.style.display = 'block';
   refs.currenciesRates.style.display = 'none';
+  refs.calculatorRef.classList.add('is-active');
+  refs.ratesRef.classList.remove('is-active');
 });
